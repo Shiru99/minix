42cc1fe8106adfa77d082fa675d4647ecc6fbb52
Shiru99 <shriram.ghadge1999@gmail.com>
2021-01-23 14:05:27 +0530

    lab 2 final

--- Lab 2/part 1/HelloWorld.c
+++ Lab 2/part 1/HelloWorld.c
@@ -1,25 +1,27 @@
-#include <stdio.h> 
-#include <sys/types.h> 
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-void forkPrint(char str[], int i, int len){
-    
-    if(i <= len){
+void printHello(char str[], int i, int len)
+{
+    if (i <= len)
+    {
         int id = fork();
-        if(id > 0){ 
+        if (id > 0)
+        {
             printf("%c %d\n", str[i], id);
-            sleep(rand()%4 + 1);
-            forkPrint(str, i+1, len);
+            sleep((rand() % 4) + 1);
+            printHello(str, i + 1, len);
         }
     }
-    return;  
+    return;
 }
 
-int main() 
-{ 
+int main()
+{
     char str[] = "Hello World";
     setbuf(stdout, NULL);
-    forkPrint(str, 0, 10);
-    return 0; 
+    printHello(str, 0, 10);
+    return 0;
 }
